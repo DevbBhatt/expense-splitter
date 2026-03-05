4c5da1a7cdf0c20027d211bf38d83af718bc6d61
Dev Bhatt <devb24303@gmail.com>
2026-03-05 21:02:17 +0530

    Create user entity

--- src/main/java/com/expensesplitter/expense_splitter/entity/User.java
+++ src/main/java/com/expensesplitter/expense_splitter/entity/User.java
@@ -1,4 +1,27 @@
 package com.expensesplitter.expense_splitter.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
 }
