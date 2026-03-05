eeb43e154c6dad4d2441e916e09eb2447e5211b8
Dev Bhatt <devb24303@gmail.com>
2026-03-05 21:44:58 +0530

    create balance entity

--- src/main/java/com/expensesplitter/expense_splitter/entity/Balance.java
+++ src/main/java/com/expensesplitter/expense_splitter/entity/Balance.java
@@ -1,4 +1,24 @@
 package com.expensesplitter.expense_splitter.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "balances")
+@Data
 public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user1_id")
+    private User user1;
+
+    @ManyToOne
+    @JoinColumn(name = "user2_id")
+    private User user2;
+
+    private Double amount;
 }
