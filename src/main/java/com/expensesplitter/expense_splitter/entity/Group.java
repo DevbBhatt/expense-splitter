d6d6702eee619eb274eb97dae0e01c09072e1f20
Dev Bhatt <devb24303@gmail.com>
2026-03-05 21:24:13 +0530

    create group entity

--- src/main/java/com/expensesplitter/expense_splitter/entity/Group.java
+++ src/main/java/com/expensesplitter/expense_splitter/entity/Group.java
@@ -1,5 +1,25 @@
 package com.expensesplitter.expense_splitter.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "user_groups")
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    private LocalDateTime createdAt;
 
 }
