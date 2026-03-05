50f998e58ea435f7f084fa312eff80532e0fbf9e
Dev Bhatt <devb24303@gmail.com>
2026-03-05 21:40:58 +0530

    create Expense Entity

--- src/main/java/com/expensesplitter/expense_splitter/entity/Expense.java
+++ src/main/java/com/expensesplitter/expense_splitter/entity/Expense.java
@@ -1,4 +1,32 @@
 package com.expensesplitter.expense_splitter.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "expenses")
+@Data
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private Double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "paid_by")
+    private User paidBy;
+
+    private String splitType;
+
+    private LocalDateTime createdAt;
 }
